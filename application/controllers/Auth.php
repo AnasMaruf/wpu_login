e00db8b6beb47d52ccbd404055a10fbc425a9331
AnasMaruf <anasmaruf1101@gmail.com>
2021-09-08 09:52:25 +0700

    Menambahkan user access menu, menu management, submenu management

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,9 +5,107 @@ class Auth extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'Login Page';
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'password', 'required|trim');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if ($user) {
+            if ($user['is_active'] == 1) {
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' =>  $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Wrong Password! 
+                    </div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                This email is not been activated! 
+                </div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            This email is not registered! 
+            </div>');
+            redirect('auth');
+        }
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => "Password don't match!",
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[3]|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name')),
+                'email' => htmlspecialchars($this->input->post('email')),
+                'image' => 'default.png',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulations! your account has been created. Please login
+            </div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        You have been Logout!
+        </div>');
+        redirect('auth');
+    }
+
+    public function blocked()
+    {
+
+        $this->load->view('auth/blocked');
     }
 }
---
+++ application/views/user/change-password.php
@@ -0,0 +1,35 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $this->session->flashdata('message'); ?>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="current_password">Current Password</label>
+                    <input type="password" class="form-control" id="current_password" name="current_password">
+                    <?= form_error('current_password', '<small class="text-danger">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <label for="password1">New Password</label>
+                    <input type="password" class="form-control" id="password1" name="password1">
+                    <?= form_error('password1', '<small class="text-danger">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <label for="password2">Confirm Password</label>
+                    <input type="password" class="form-control" id="password2" name="password2">
+                    <?= form_error('password2', '<small class="text-danger">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit">Change Password</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
